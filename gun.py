2c2629435829a90f4c6dd6b4cb7191043ef315ad
chenhua <chenhua22@outlook.com>
2017-03-25 20:55:08 +0800

    add gunicorn file

---
+++ gun.py
@@ -0,0 +1,17 @@
+import os
+import gevent.monkey
+gevent.monkey.patch_all()
+
+# import multiprocessing
+
+debug = False
+loglevel = 'notice'
+bind = '0.0.0.0:5000'
+pidfile = '/var/run/gunicorn.pid'
+logfile = '/var/log/gunicorn_buhuipao.log'
+
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
+worker_class = 'gunicorn.workers.ggevent.GeventWorker'
+
+x_forwarded_for_header = 'X-FORWARDED-FOR'
